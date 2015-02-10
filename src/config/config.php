8b7fe95d5589c0c4698c1f03d3544ccea639b201
Steve Bauman <steven_bauman_7@hotmail.com>
2015-02-10 11:22:32 -0500

    Updated configuration documentation

--- src/config/config.php
+++ src/config/config.php
@@ -1,50 +1,108 @@
 <?php
 
+/*
+ * The Corp configuration file
+ *
+ * @package Stevebauman/Corp
+ */
 return array(
 
+	/*
+	 * AdLDAP Configuration
+	 */
 	'adldap_config' => array(
+
+		/*
+		 * The account suffix of the domain
+		 */
 		'account_suffix' => "@domain.org",
 
-		'domain_controllers' => array("domain.org", "domain2.org"), // An array of domains may be provided for load balancing.
+		/*
+		 * An array of domains for load balancing
+		 *
+		 * Must be array
+		 */
+		'domain_controllers' => array("domain.org", "domain2.org"),
 
-		'base_dn' => '',//'DC=domain,DC=local',
+		/*
+		 * The base distinguished name
+		 *
+		 * ex. DC=domain,DC=local
+		 */
+		'base_dn' => '',
 
+		/*
+		 * The administrator username
+		 */
 		'admin_username' => 'admin',
 
+		/*
+		 * The administrator password
+		 */
 		'admin_password' => '12345',
-		'real_primary_group' => true, // Returns the primary group (an educated guess).
 
-		//Both can be false if no SSL/TLS is used on server
+		/*
+		 * Returns the primary group (an educated guess)
+		 */
+		'real_primary_group' => true,
+
+		/*
+		 * Both can be false if no SSL/TLS is used on server
+		 *
+		 * Must be boolean
+		 */
 		'use_ssl' => false, // If TLS is true this MUST be false.
 		'use_tls' => false, // If SSL is true this MUST be false.
 
 		'recursive_groups' => true,
+
 	),
 
 	/*
-	* You can edit some options here for some customization of results on functions
-	*/
+	 * Corp Configuration
+	 *
+	 * You can edit some options here for some customization of results on functions
+	 */
 	'options' => array(
-		// Function name (ex Corp::allUsers())
+
+		/*
+		 * The Corp::users() configuration
+		 */
 		'users' => array(
+
 			/*
-			* When using function Corp::allUsers(), certain group types can be excluded so service accounts and other non-human accounts don't mess up your results
-			*/
+			 * When using function Corp::users(), certain group types can be excluded so service accounts and
+			 * other non-human accounts so they don't cloud your results
+			 *
+			 * Must be array
+			 */
 			'excluded_user_types' => array(
 				'Service Accounts', 
 				'Microsoft Exchange System Objects',
 			),
+
 			/*
-                         *  You can also exclude specific groups from your user results
-                         */
+			 * You can also exclude specific groups from your user results
+			 *
+			 * Must be array
+			 */
 			'excluded_user_groups' => array(
 				'corp'
 			),
                     
 		),
 
+		/*
+		 * The Corp::computers() configuration
+		 */
 		'computers' => array(
-			'folder' => array('Computers'), // Must leave folder as array
+
+			/*
+			 * The folder to search for computers in.
+			 *
+			 * Must be array
+			 */
+			'folder' => array('Computers'),
 		),
 	),
 
